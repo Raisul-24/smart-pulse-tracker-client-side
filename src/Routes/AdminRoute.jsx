c8b1b1e7d74a268dc73d9454fb47031308b7d675
Raisul Islam Hridoy <raisul15-3813@diu.edu.bd>
2023-11-27 23:14:37 +0600

    add admin route and hook

---
+++ src/Routes/AdminRoute.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+
+import { Navigate, useLocation } from 'react-router-dom';
+import UseAuth from '../hooks/UseAuth';
+import UseAdmin from '../hooks/UseAdmin';
+
+const AdminRoute = ({children}) => {
+   const { user, loading } = UseAuth();
+   const [isAdmin, isAdminLoading] = UseAdmin();
+   const location = useLocation();
+
+   if (loading || isAdminLoading) {
+       return <progress className="progress w-56"></progress>
+   }
+
+   if (user && isAdmin) {
+       return children;
+   }
+
+   return <Navigate to="/" state={{ from: location }} replace></Navigate>
+}
+
+export default AdminRoute;
---
+++ src/hooks/UseAdmin.jsx
@@ -0,0 +1,21 @@
+
+import UseAuth from './UseAuth';
+import UseAxiosSecure from './UseAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
+
+const UseAdmin = () => {
+   const {user, loading} = UseAuth()
+   const axiosSecure = UseAxiosSecure();
+   const {data: isAdmin, isPending: isAdminLoading} = useQuery({
+      queryKey: [user?.email, 'isAdmin'],
+      enabled: !loading,
+      queryFn: async() =>{
+         const res = await axiosSecure.get(`/users/admin/${user.email}`);
+         // console.log(res.data);
+         return res.data?.admin;
+      }
+   })
+   return [isAdmin, isAdminLoading]
+};
+
+export default UseAdmin;
